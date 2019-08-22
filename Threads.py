52301b212e8d53a8c054e8528c51d28a67b77bdc
brunofelipecs <50154170+brunofelipecs@users.noreply.github.com>
2019-08-22 16:56:24 -0300

    Add files via upload

---
+++ Threads.py
@@ -0,0 +1,31 @@
+def fatorial(valor):
+    fat = valor
+    for aux in range(1,valor):
+        fat *= aux
+    return fat
+
+def primo(valor):
+    primo = 0
+    for div in range(1,valor+1):
+        if(valor % div == 0):
+            primo += 1
+    if primo == 2: return True
+    else: return False
+
+valor = int(input("Informe um númmero: "))
+
+arq = open('VALORES_PRIMOS.TXT', 'w')
+arq2 = open('VALORE_NAO_PRIMOS.TXT', 'w')
+
+for i in range(1,valor+1):
+    if primo(i) == True:
+        arq.write("{0} é primo e seu fatorial é: {1}!\n\n".format(i, fatorial(i)))
+        arq.write("-" * 100)
+        arq.write("\n\n")
+    else:
+        arq2.write("{0} não é primo!\n\n".format(i))
+        arq2.write("-" * 100)
+        arq2.write("\n\n")
+
+arq.close()
+arq2.close()
